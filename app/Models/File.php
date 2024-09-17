9fabe12a9952eacf1c31f4345e039985f0166b4f
m7moud <mg6783256@gmail.com>
2024-09-17 10:11:22 +0300

    models

---
+++ app/Models/File.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class File extends Model
+{
+    use HasFactory, HasTranslations, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public $translatable = ['address'];
+}
